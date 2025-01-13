331aa69cd48f8c1eb5ad7886db3ba33f71e1ce52
rainy <rainy2258@gmail.com>
2025-01-13 09:49:54 +0800

    引入window_manager包并初始化页面结构

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flip Clock'),
+      ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        child: Container(
+          color: Colors.pink,
+          child: Column(
+            children: [
+                Flexible(
+                    child: Container(
+                        color: Colors.orange,
+                        child: Center(
+                            child: Text('body'),
+                        ),
+                    ),
+                ),
+                Container(
+                    color: Colors.yellow,
+                    child: SizedBox(
+                        height: 40,
+                        child: Text('bottom'),
+                    ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
